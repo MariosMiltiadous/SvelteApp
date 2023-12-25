3b81df6fac04e637753fd61e97ac0a92d5d35ae6
Marios <46002841+MariosMiltiadous@users.noreply.github.com>
2023-12-25 22:37:28 +0200

    Create basics

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Create a simple blog for learning the basics of Svelte
+
 # This repo is no longer maintained. Consider using `npm init vite` and selecting the `svelte` option or — if you want a full-fledged app framework — use [SvelteKit](https://kit.svelte.dev), the official application framework for Svelte.
 
 ---
--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,35 @@
 <script>
-	export let name;
+	import Modal from "./Modal.svelte";
+
+	let people = [
+		{ name: "yoshi", beltColour: "black", age: 25, id: 1 },
+		{ name: "mario", beltColour: "orange", age: 45, id: 2 },
+		{ name: "luigi", beltColour: "brown", age: 35, id: 3 },
+	];
+
+	const handleDelete = (id) => {
+		people = people.filter((person) => person.id != id);
+	};
+
 </script>
 
+<Modal/>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			{#if person.beltColour === 'black'}
+			<p><strong>MASTER NINJA</strong></p>
+			{/if}
+			<p>
+				{person.age} years old, {person.beltColour} belt. - {person.id}
+			</p>
+			<button on:click={() => handleDelete(person.id)}> Delete </button>
+		</div>
+	{:else}
+		<p>There are no people to show...</p>
+	{/each}
 </main>
 
 <style>
@@ -15,11 +40,20 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
+	/* h4 {
+		color:blue;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	} */
+
+	:global(img) {
+		opacity: 0.9;
+		transition: all 0.2s;
+	}
+	:global(img):hover {
+		opacity: 1;
+		transform: scale(1.04);
 	}
 
 	@media (min-width: 640px) {
